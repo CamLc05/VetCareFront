e09c56fc0432ed433f78d4e86e208603bf9edbc0
Ruth17M <manriquezruth2004@gmail.com>
2025-06-22 22:05:15 -0600

    App funciona con BD prueba

--- Views/Componentes/PacienteCardView.xaml.cs
+++ Views/Componentes/PacienteCardView.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using Veterinaria.Models;
 
 namespace Veterinaria.Views.Componentes;
 
@@ -11,5 +13,19 @@ public partial class PacienteCardView : ContentView
     public PacienteCardView()
     {
         InitializeComponent();
+    }
+    public static readonly BindableProperty PacienteProperty =
+        BindableProperty.Create(nameof(Paciente), typeof(Paciente), typeof(PacienteCardView), propertyChanged: OnPacienteChanged);
+
+    public Paciente Paciente
+    {
+        get => (Paciente)GetValue(PacienteProperty);
+        set => SetValue(PacienteProperty, value);
+    }
+
+    private static void OnPacienteChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var control = (PacienteCardView)bindable;
+        control.BindingContext = control;
     }
 }
